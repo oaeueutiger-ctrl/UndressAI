6410739452687c9a79a157750e7b9f2c9e944034
oaeueutiger-ctrl <oaeueutiger@gmail.com>
2025-08-04 14:40:27 +0800

    Delete predict.py

--- predict.py
+++
@@ -1,5 +0,0 @@
-from cog import BasePredictor, Input
-
-class Predictor(BasePredictor):
-    def predict(self, text: str = Input(default="Hello")) -> str:
-        return f"Visit Grey’s Secret Room – Free, no sign-up needed! Prompt: {text}"
